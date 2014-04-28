092cb5f48356e11fce4f737bb79c710b2876a086
Chris Marr <cdabone@gmail.com>
2014-04-28 12:50:28 +0100

    Expanded framework to include EEPROM configuration reading

--- src/Config.cpp
+++ src/Config.cpp
@@ -6,18 +6,38 @@
  */
 
 #include "Config.h"
+#include "Defaults.h"
+
+#include <EEPROM.h>
 
 CConfig::CConfig() {
 	// Set Configuration to default values
-
-
+	m_nSerialBaudRate = DEFAULT_BAUD;
 }
 
 CConfig::~CConfig() {
 	// TODO Auto-generated destructor stub
 }
 
+int CConfig::ReadEEPROMBuffer(int &nAddr, void *pDest, size_t len)
+{
+	size_t n;
+	for (n=0; n<len; n++)
+		*(((uint8_t*)pDest)+n)=EEPROM.read(nAddr+n);
+
+	nAddr += len;
+	return (int)len;
+}
+
 void CConfig::RetrieveSettings()
 {
 	//	Retrieve settings from EEPROM
+	int i=0;
+	int nVer;
+	ReadEEPROMBuffer(i, &nVer, sizeof(nVer));
+
+	if(nVer == CONFIG_VER)
+	{
+		ReadEEPROMBuffer(i,&m_nSerialBaudRate,sizeof(m_nSerialBaudRate));
+	}
 }
--- src/Config.h
+++ src/Config.h
@@ -8,11 +8,24 @@
 #ifndef CONFIG_H_
 #define CONFIG_H_
 
+#include <stdlib.h>
+
+
+//	Development version...
+#define	CONFIG_VER	0x00
+
 class CConfig {
 public:
 	CConfig();
 	virtual ~CConfig();
 	void RetrieveSettings();
+	void StoreSettings();
+
+	long getBaudRate(){return m_nSerialBaudRate;}
+private:
+	int ReadEEPROMBuffer(int &nAddr, void *pDest, size_t len);
+
+	long m_nSerialBaudRate;
 };
 
 #endif /* CONFIG_H_ */

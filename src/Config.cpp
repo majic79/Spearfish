003cbc8626e5c5eef7010f6575ce9d21a3128d74
Chris Marr <cdabone@gmail.com>
2014-04-30 10:16:07 +0100

    Changed Config to optionally read/write EEPROM to maintain EEPROM order automatically

--- src/Config.cpp
+++ src/Config.cpp
@@ -10,6 +10,7 @@
 
 #include "EEPROM/EEPROM.h"
 
+
 CConfig::CConfig() {
 	// Set Configuration to default values
 	m_nSerialBaudRate = DEFAULT_BAUD;
@@ -20,25 +21,46 @@ CConfig::~CConfig() {
 	// We shouldn't need to do anything in here as we use this as a static instance for the life of the embedded device
 }
 
+//Could probably use the below macro to achieve the same...
+//#define ReadWriteEEPROMBuffer(a,b,c,d) ({int n;if(d){n=ReadEEPROMBuffer(a,b,c);}else{n=WriteEEPROMBuffer(a,b,c);} n;})
+int CConfig::ReadWriteEEPROMBuffer(int &nAddr, void *pData, size_t len, bool bRead)
+{
+	int retlen;
+	if(bRead != EEPROM_WRITE)
+		retlen = ReadEEPROMBuffer(nAddr, pData, len);
+	else
+		retlen = WriteEEPROMBuffer(nAddr, pData, len);
+
+	return retlen;
+}
+
 int CConfig::ReadEEPROMBuffer(int &nAddr, void *pDest, size_t len)
 {
 	size_t n;
 	for (n=0; n<len; n++)
 		*(((uint8_t*)pDest)+n)=EEPROM.read(nAddr+n);
 
-	nAddr += len;
 	return (int)len;
 }
 
-void CConfig::RetrieveSettings()
+int CConfig::WriteEEPROMBuffer(int &nAddr, void *pDest, size_t len)
+{
+	size_t n;
+	for (n=0; n<len; n++)
+		EEPROM.write(nAddr+n, *(((uint8_t*)pDest)+n));
+
+	return (int)len;
+}
+
+void CConfig::ReadWriteSettings(bool bRead)
 {
 	//	Retrieve settings from EEPROM
 	int i=0;
 	int nVer;
-	ReadEEPROMBuffer(i, &nVer, sizeof(nVer));
+	i+=ReadWriteEEPROMBuffer(i, &nVer, sizeof(nVer), bRead);
 
 	if(nVer == CONFIG_VER)
 	{
-		ReadEEPROMBuffer(i,&m_nSerialBaudRate,sizeof(m_nSerialBaudRate));
+		i+=ReadWriteEEPROMBuffer(i,&m_nSerialBaudRate,sizeof(m_nSerialBaudRate), bRead);
 	}
 }
--- src/Config.h
+++ src/Config.h
@@ -13,19 +13,22 @@
 //	Development version...
 #define	CONFIG_VER	0x00
 
+#define EEPROM_READ true
+#define EEPROM_WRITE false
+
 class CConfig {
 public:
 	CConfig();
 	virtual ~CConfig();
-	void RetrieveSettings();
-	void StoreSettings();
+	void ReadWriteSettings(bool bRead);
 
 	//	Baud rate for serial comms
-	unsigned long getBaudRate(){return m_nSerialBaudRate;}
+	unsigned long getBaudRate(){return m_nSerialBaudRate;};
 
 private:
+	int ReadWriteEEPROMBuffer(int &nAddr, void *pData, size_t len, bool bRead);
 	int ReadEEPROMBuffer(int &nAddr, void *pDest, size_t len);
-
+	int WriteEEPROMBuffer(int &nAddr, void *pSource, size_t len);
 	unsigned long m_nSerialBaudRate;
 };
 
